53a12f4b114adfb99ac7ca36c4968a02b91abad2
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2022-01-25 16:39:27 -0300

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,45 @@
+function buttonUpper() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+function definirBotao() {
+    if (window.scrollY === 0) {
+        document.querySelector('.button-upper').style.display = 'none';
+    } else {
+        document.querySelector('.button-upper').style.display = 'block';
+    }
+}
+
+window.addEventListener('scroll', definirBotao);
+
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event){
+    event.preventDefault();
+    const element = event.target;
+    const to = document.querySelector('#Gallery').offsetTop;
+    const id = element.getAttribute('href');
+
+    window.scroll({
+        top: to - 100,
+        behavior: "smooth"
+    });
+}
+
+function botaoMenu() {
+    let menu = document.querySelector('.nav-list');
+
+    if (menu.style.right == '-200vw') {
+      menu.style.right = '0';
+    } else {
+      menu.style.right = '-200vw';
+    }
+
+  };
